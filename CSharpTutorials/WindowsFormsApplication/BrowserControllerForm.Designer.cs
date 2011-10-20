17765aba75326cd6fba18b3e0951c05440bf0baf
v-elluo <v-elluo@expedia.com>
2011-10-19 19:26:39 -0700

    Add tutorial project Signed-off-by: v-elluo <v-elluo@expedia.com>

---
+++ CSharpTutorials/WindowsFormsApplication/BrowserControllerForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WindowsFormsApplication
+{
+    partial class BrowserControllerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartBrowser = new System.Windows.Forms.Button();
+            this.pgbBrowser = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnStartBrowser
+            // 
+            this.btnStartBrowser.Location = new System.Drawing.Point(13, 13);
+            this.btnStartBrowser.Name = "btnStartBrowser";
+            this.btnStartBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnStartBrowser.TabIndex = 0;
+            this.btnStartBrowser.Text = "Start";
+            this.btnStartBrowser.UseVisualStyleBackColor = true;
+            this.btnStartBrowser.Click += new System.EventHandler(this.btnStartBrowser_Click);
+            // 
+            // pgbBrowser
+            // 
+            this.pgbBrowser.Location = new System.Drawing.Point(95, 12);
+            this.pgbBrowser.Name = "pgbBrowser";
+            this.pgbBrowser.Size = new System.Drawing.Size(185, 23);
+            this.pgbBrowser.TabIndex = 1;
+            // 
+            // BrowserControllerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 49);
+            this.Controls.Add(this.pgbBrowser);
+            this.Controls.Add(this.btnStartBrowser);
+            this.Name = "BrowserControllerForm";
+            this.Text = "Windows Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartBrowser;
+        private System.Windows.Forms.ProgressBar pgbBrowser;
+    }
+}
---
+++ CSharpTutorials/WindowsFormsApplication/BrowserControllerForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SHDocVw;
+
+namespace WindowsFormsApplication
+{
+    public partial class BrowserControllerForm : Form
+    {
+        public BrowserControllerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartBrowser_Click(object sender, EventArgs e)
+        {
+            InternetExplorer ie = new InternetExplorer();
+            ie.ProgressChange += new DWebBrowserEvents2_ProgressChangeEventHandler(ie_ProgressChange);
+            object Empty = 0;
+            object URL = "http://bdtools.sb.karmalab.net/envstatus/envstatus.cgi?";
+
+            ie.Visible = true;
+            ie.Navigate2(ref URL, ref Empty, ref Empty, ref Empty, ref Empty);
+
+            while (ie.Busy)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            ie.Quit();
+        }
+
+        void ie_ProgressChange(int Progress, int ProgressMax)
+        {
+            Console.Write(Progress);
+        }
+    }
+}
