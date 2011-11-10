11bc09bdcc0a6b2daba9249dc4e0999de0ee6100
Elvin (Home Windows7) <elvin.luo@gmail.com>
2011-11-10 23:21:28 +0800

    Add solution to generate soft case automatically. Signed-off-by: Elvin (Home Windows7) <elvin.luo@gmail.com>

---
+++ SoftCaseGenerator/SoftCaseGenerator/Input.cs
@@ -0,0 +1,51 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Input.cs" company="Expedia">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SoftCaseGenerator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Input
+    {
+        /// <summary>
+        /// Used to save all valid available config items, values and node
+        /// </summary>
+        private List<string[]> validConfigItems;
+
+        /// <summary>
+        /// Used to save all invalid available config items, values and node 
+        /// </summary>
+        private List<string[]> invalidConfigItems;
+
+        /// <summary>
+        /// Initializes a new instance of the Input class.
+        /// </summary>
+        /// <param name="validConfigItems">Pass in valid</param>
+        /// <param name="invalidConfigItems">Pass in invalid</param>
+        public Input(List<string[]> validConfigItems, List<string[]> invalidConfigItems)
+        {
+            this.validConfigItems = validConfigItems;
+            this.invalidConfigItems = invalidConfigItems;
+        }
+
+        /// <summary>
+        /// Begin to generate
+        /// </summary>
+        public void GenerateSoftCase()
+        {
+            foreach (string[] item in validConfigItems)
+            {
+            }
+        }
+
+    }
+}
---
+++ SoftCaseGenerator/SoftCaseGenerator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SoftCaseGenerator
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new SoftCaseGenerator());
+
+            string[] ratePlanContractType = new string[] { "RatePlanContractType", "1/2/3", ".Merchan/.Agency/.Flex" };
+            string[] ratePlanType = new string[] { "RatePlanType", "1/2/3", ".Standalone/.Package/.Corporate" };
+            List<string[]> valid = new List<string[]>();
+            valid.Add(ratePlanContractType);
+            valid.Add(ratePlanType);
+            
+            Input input = new Input(valid, null);
+        }
+    }
+}
