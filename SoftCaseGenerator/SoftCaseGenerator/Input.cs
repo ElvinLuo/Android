fa834d5c2b6e0dc79dd955d5d4ea1d89b7e2de60
v-elluo <v-elluo@expedia.com>
2011-11-11 02:09:23 -0800

    Make it able to list all cases from config. Signed-off-by: v-elluo <v-elluo@expedia.com>

--- SoftCaseGenerator/SoftCaseGenerator/Input.cs
+++ SoftCaseGenerator/SoftCaseGenerator/Input.cs
@@ -16,6 +16,9 @@ namespace SoftCaseGenerator
     /// </summary>
     public class Input
     {
+        private string prefix = "CheckUI.";
+        private string suffix = "";
+
         /// <summary>
         /// Used to save all valid available config items, values and node
         /// </summary>
@@ -26,24 +29,85 @@ namespace SoftCaseGenerator
         /// </summary>
         private List<string[]> invalidConfigItems;
 
+        private Dictionary<string, string> softCases;
+
         /// <summary>
         /// Initializes a new instance of the Input class.
         /// </summary>
         /// <param name="validConfigItems">Pass in valid</param>
         /// <param name="invalidConfigItems">Pass in invalid</param>
-        public Input(List<string[]> validConfigItems, List<string[]> invalidConfigItems)
+        public Input(
+            List<string[]> validConfigItems,
+            List<string[]> invalidConfigItems)
         {
             this.validConfigItems = validConfigItems;
             this.invalidConfigItems = invalidConfigItems;
         }
 
+        public Dictionary<string, string> GetAllSoftCasesFromConfig()
+        {
+            if (softCases == null)
+            {
+                GetCombination(0);
+            }
+            return softCases;
+        }
+
         /// <summary>
         /// Begin to generate
         /// </summary>
-        public void GenerateSoftCase()
+        public void GetCombination(int processingIndex)
         {
-            foreach (string[] item in validConfigItems)
+            Dictionary<string, string> temp;
+            if (softCases == null)
             {
+                temp = new Dictionary<string, string>();
+            }
+            else
+            {
+                temp = new Dictionary<string, string>(softCases);
+                softCases.Clear();
+            }
+            string[] configItemValues = validConfigItems[processingIndex][1].Split(new char[] { '/' });
+            string[] configItemNodeNames = validConfigItems[processingIndex][2].Split(new char[] { '/' });
+
+            if (temp.Count == 0)
+            {
+                softCases = new Dictionary<string, string>();
+                for (int i = 0; i < configItemValues.Length; i++)
+                {
+                    string configItemNodeName = configItemNodeNames[i];
+                    string configItemValue = configItemValues[i];
+                    softCases.Add(prefix + configItemNodeName, configItemValue);
+                }
+            }
+            else
+            {
+                foreach (KeyValuePair<string, string> softCase in temp)
+                {
+                    for (int i = 0; i < configItemValues.Length; i++)
+                    {
+                        string configItemNodeName = configItemNodeNames[i];
+                        string configItemValue = configItemValues[i];
+                        string softCaseName;
+                        if (processingIndex != validConfigItems.Count - 1)
+                        {
+                            softCaseName = softCase.Key + configItemNodeName;
+                        }
+                        else
+                        {
+                            softCaseName = softCase.Key + configItemNodeName + suffix;
+                        }
+                        softCases.Add(
+                            softCaseName,
+                            softCase.Value + "\t" + configItemValue);
+                    }
+                }
+            }
+
+            if (processingIndex != validConfigItems.Count - 1)
+            {
+                GetCombination(processingIndex + 1);
             }
         }
 
--- SoftCaseGenerator/SoftCaseGenerator/Program.cs
+++ SoftCaseGenerator/SoftCaseGenerator/Program.cs
@@ -17,13 +17,41 @@ namespace SoftCaseGenerator
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new SoftCaseGenerator());
 
-            string[] ratePlanContractType = new string[] { "RatePlanContractType", "1/2/3", ".Merchan/.Agency/.Flex" };
-            string[] ratePlanType = new string[] { "RatePlanType", "1/2/3", ".Standalone/.Package/.Corporate" };
+            string[] pricing = new string[] {
+                "PricingModel",
+                "PDP/OBP/PPP",
+                "PDP./OBP./PPP." };
+            string[] lar = new string[] {
+                "LAREnabled",
+                "LAR/NonLAR", 
+                "LAR./NonLAR." };
+            string[] los = new string[] {
+                "LOSEnabled", 
+                "True/False",
+                "LOS_/" };
+            string[] ratePlanType = new string[] {
+                "RatePlanType", 
+                "Standalone/Package/Corporate", 
+                "/Package_/Corporate_" };
+            string[] ratePlanContractType = new string[] {
+                "RatePlanContractType", 
+                "Merchant/Agency/Flex",
+                "Merchant/Agency/Flex" };
+            string[] targetRatePlanContractType = new string[] {
+                "RatePlanContractType", 
+                "Merchant/Agency/Flex",
+                "ToMerchant/ToAgency/ToFlex" };
+
             List<string[]> valid = new List<string[]>();
-            valid.Add(ratePlanContractType);
+            valid.Add(pricing);
+            valid.Add(lar);
+            valid.Add(los);
             valid.Add(ratePlanType);
-            
+            valid.Add(ratePlanContractType);
+            valid.Add(targetRatePlanContractType);
+
             Input input = new Input(valid, null);
+            Dictionary<string, string> softCases = input.GetAllSoftCasesFromConfig();
         }
     }
 }
