2dbbd8803180899970c995f7b30d897708780587
Elvin (Home Windows7) <elvin.luo@gmail.com>
2011-11-15 00:19:32 +0800

    Add serializer. Signed-off-by: Elvin (Home Windows7) <elvin.luo@gmail.com>

---
+++ SoftCaseGenerator/SoftCaseGenerator/Serializer.cs
@@ -0,0 +1,56 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Serializer.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SoftCaseGenerator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+    using System.IO;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Serializer
+    {
+        private static Serializer instance;
+
+        public static Serializer CreateInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Serializer();
+            }
+            return instance;
+        }
+
+        public void SerializeToXML(object instance, Type type, string file)
+        {
+            try
+            {
+                XmlSerializer x = new XmlSerializer(type);
+                TextWriter text = new StreamWriter(file);
+                x.Serialize(text, instance);
+                text.Flush();
+                text.Close();
+            }
+            catch (Exception exception)
+            { Console.WriteLine(exception.Message); }
+        }
+
+        public object DeserializeFromXML(Type type, string file)
+        {
+            XmlSerializer x = new XmlSerializer(type);
+            TextReader text = new StreamReader(file);
+            object resut = x.Deserialize(text);
+            text.Close();
+            return resut;
+        }
+
+    }
+}
---
+++ SoftCaseGenerator/SoftCaseGenerator/SoftTest.cs
@@ -0,0 +1,66 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SoftTest.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SoftCaseGenerator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class SoftTest
+    {
+        public int id;
+        public string testTeam;
+        public string category;
+        public int risktier;
+        public Invoke invoke;
+        public List<string> parameters;
+        public List<Data> testData;
+
+        public SoftTest()
+        {
+            id = 77091;
+            testTeam = "Lodging Inventory Systems";
+            category = "Regression";
+            risktier = 2;
+            invoke = new Invoke("", "");
+            parameters = new List<string>();
+            testData = new List<Data>();
+            testData.Add(new Data("", "", ""));
+            testData.Add(new Data("", "", ""));
+        }
+    }
+
+    public struct Invoke
+    {
+        public string module;
+        public string method;
+
+        public Invoke(string module, string method)
+        {
+            this.module = "HotelTest.dll";
+            this.method = "Expedia.Automation.Test.Hotels.ExpediaServiceFee.Functional.ExpediaServiceFeeBulkUpdate";
+        }
+    }
+
+    public struct Data
+    {
+        public string dataName;
+        public string defaultValue;
+        public string stripingValues;
+
+        public Data(string dataName, string defaultValue, string stripingValues)
+        {
+            this.dataName = "ContainAgencyHotel";
+            this.defaultValue = "true";
+            this.stripingValues = string.Empty;
+        }
+    }
+}
