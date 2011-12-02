3c32ec58ff205aadf622408dec7fb8def4236962
v-elluo <v-elluo@expedia.com>
2011-12-02 00:16:57 -0800

    Signed-off-by: v-elluo <v-elluo@expedia.com>

--- CSharpTutorials/WindowsFormsApplication/Program.cs
+++ CSharpTutorials/WindowsFormsApplication/Program.cs
@@ -13,9 +13,40 @@ namespace WindowsFormsApplication
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new BrowserControllerForm());
+            Dictionary<string, string> expected = new Dictionary<string, string>
+            { { "200021792", "289400" },
+            { "200021030", "Automation 000711182612" } };
+
+            Dictionary<string, string> actual = new Dictionary<string, string>
+            {{ "200021030", "Automation 000711182612" },
+            { "200021792", "289400" }};
+
+            bool result = IsEquals(expected, actual);
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new BrowserControllerForm());
         }
+
+        static bool IsEquals(Dictionary<string, string> actual, Dictionary<string, string> expected)
+        {
+            if (actual.Count != expected.Count)
+            {
+                return false;
+            }
+            else
+            {
+                foreach (KeyValuePair<string, string> pair in actual)
+                {
+                    if (!expected.Contains(pair))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
     }
+
 }
