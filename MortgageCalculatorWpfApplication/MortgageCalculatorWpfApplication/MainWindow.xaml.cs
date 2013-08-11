e151870a5a0c7fdebee2f98c42c1489805df66a6
ElvinLuo <elvin.luo@gmail.com>
2013-08-11 22:43:48 +0800

    Adding MortageCalculator project Signed-off-by: ElvinLuo <elvin.luo@gmail.com>

---
+++ MortgageCalculatorWpfApplication/MortgageCalculatorWpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MortgageCalculatorWpfApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            MortgageCalculator.MortgageCalculator mortgageCalculator = new MortgageCalculator.MortgageCalculator();
+            this.tbMonthlyMortgage.Text = mortgageCalculator.GetMonthlyMortgage().ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MortgageCalculator.MortgageCalculator mortgageCalculator = new MortgageCalculator.MortgageCalculator();
+            this.lblLeftOverMortgage.Content =
+                "个月后剩余本金：" + mortgageCalculator.GetLeftOverMortgate(int.Parse(this.tbPaidNumberOfMonths.Text));
+            this.lblPaidMortgage.Content =
+                "个月还本金：" + mortgageCalculator.GetPaidMortgate(int.Parse(this.tbPaidNumberOfMonths.Text));
+            this.finalPaidMoney.Content = "共还款：" + mortgageCalculator.GetTotalMoneyNeedToPay(
+                1260000,
+                360,
+                int.Parse(this.alreadyPaidMonths.Text),
+                double.Parse(this.totalPrepaidMoney.Text));
+        }
+    }
+}
---
+++ MortgageCalculatorWpfApplication/MortgageCalculatorWpfApplication/MortgageCalculator.cs
@@ -0,0 +1,70 @@
+﻿namespace MortgageCalculator
+{
+    using System;
+
+    public class MortgageCalculator
+    {
+        private int totalNumberOfYears = 30;
+        private int totalNumberOfMonths;
+        private int currentNumberOfMonths;
+
+        private double totalMortgate = 1260000;
+        private double rateOfTheYear = 0.0655;
+        private double rateOfTheMonth;
+        private double currentMonthlyRate;
+        private double discount = 0.85;
+
+        public MortgageCalculator()
+        {
+            totalNumberOfMonths = 12 * totalNumberOfYears;
+            rateOfTheMonth = rateOfTheYear / 12;
+            currentMonthlyRate = rateOfTheMonth * discount;
+        }
+
+        public double GetMonthlyMortgage()
+        {
+            double ratePow = Math.Pow((1 + currentMonthlyRate), totalNumberOfMonths);
+            return totalMortgate * currentMonthlyRate * (ratePow / (ratePow - 1));
+        }
+
+        public double GetMonthlyMortgage(double totalMortgate, int totalNumberOfMonths)
+        {
+            double ratePow = Math.Pow((1 + currentMonthlyRate), totalNumberOfMonths);
+            return totalMortgate * currentMonthlyRate * (ratePow / (ratePow - 1));
+        }
+
+        public double GetTotalMoneyNeedToPay(double totalMortgate, int totalNumberOfMonths)
+        {
+            return totalNumberOfMonths * GetMonthlyMortgage(totalMortgate, totalNumberOfMonths);
+        }
+
+        public double GetTotalMoneyNeedToPay(double totalMortgate, int totalNumberOfMonths, int currentMonth, double prepaidMoney)
+        {
+            double monthlyMortgage = GetMonthlyMortgage(totalMortgate, totalNumberOfMonths);
+            double alreadyPaidMonthly = currentMonth * monthlyMortgage;
+            double faxi = 0;
+            if (currentMonth <= 36)
+            {
+                faxi = 3 * (monthlyMortgage - GetPaidMortgate(currentMonth));
+            }
+            double leftover = GetLeftOverMortgate(currentMonth) - prepaidMoney;
+            return alreadyPaidMonthly + prepaidMoney + faxi + GetTotalMoneyNeedToPay(leftover, totalNumberOfMonths - currentMonth);
+        }
+
+        public double GetLeftOverMortgate(int paidNumberOfMonths)
+        {
+            double currentMonthlyRatePlus1 = currentMonthlyRate + 1;
+            double totalNumberOfMonthsPow = Math.Pow(currentMonthlyRatePlus1, totalNumberOfMonths);
+            double paidNumberOfMonthsPow = Math.Pow(currentMonthlyRatePlus1, paidNumberOfMonths);
+            return totalMortgate * (totalNumberOfMonthsPow - paidNumberOfMonthsPow) / (totalNumberOfMonthsPow - 1);
+        }
+
+        public double GetPaidMortgate(int paidNumberOfMonths)
+        {
+            double currentMonthlyRatePlus1 = currentMonthlyRate + 1;
+            double totalNumberOfMonthsPow = Math.Pow(currentMonthlyRatePlus1, totalNumberOfMonths) - 1;
+            double paidNumberOfMonthsPow = Math.Pow(currentMonthlyRatePlus1, paidNumberOfMonths - 1);
+            return totalMortgate * currentMonthlyRate * paidNumberOfMonthsPow / totalNumberOfMonthsPow;
+        }
+    }
+}
